adf724c2d6949276283ea99f42c72509ece52e83
Brian Bechtel <bbechtel@google.com>
2024-12-07 23:36:49 -0800

    Day 6: Part 2

--- day6/day6.go
+++ day6/day6.go
@@ -2,6 +2,7 @@ package day6
 
 import (
 	. "advent/util"
+	"advent/util/set"
 )
 
 const (
@@ -23,13 +24,28 @@ func Part1(filename string) int64 {
 		}
 		table = append(table, chars)
 	}
-	row, col := findStart(table)
-	return countCells(table, row, col)
+	row, col, direction := findStart(table)
+	return countCells(table, row, col, direction)
 }
 
-func countCells(table [][]uint8, row int, col int) int64 {
+func Part2(filename string) int64 {
+	var table [][]uint8
+	scanner, closeFunc := NewScanner(filename)
+	defer closeFunc()
+	for scanner.Scan() {
+		s := scanner.Text()
+		chars := make([]uint8, 0, len(s))
+		for i := 0; i < len(s); i++ {
+			chars = append(chars, s[i])
+		}
+		table = append(table, chars)
+	}
+	row, col, direction := findStart(table)
+	return countCycles(table, row, col, direction)
+}
+
+func countCells(table [][]uint8, row, col, direction int) int64 {
 	var count int64
-	direction := Up
 	for 0 <= row && row < len(table) && 0 <= col && col < len(table[row]) {
 		if table[row][col] == '#' {
 			row, col = reverse(row, col, direction)
@@ -41,6 +57,45 @@ func countCells(table [][]uint8, row int, col int) int64 {
 		row, col = advance(row, col, direction)
 	}
 	return count
+}
+
+func countCycles(table [][]uint8, startRow, startCol, startDirection int) int64 {
+	countCells(table, startRow, startCol, startDirection)
+	count := int64(0)
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			if table[i][j] == 'X' && !(i == startRow && j == startCol) {
+				table[i][j] = '#'
+				if isCycle(table, startRow, startCol, startDirection) {
+					count++
+				}
+				table[i][j] = 'X'
+			}
+		}
+	}
+	return count
+}
+
+type pos struct {
+	row, col, direction int
+}
+
+func isCycle(table [][]uint8, row int, col int, direction int) bool {
+	positions := set.New[pos]()
+	for 0 <= row && row < len(table) && 0 <= col && col < len(table[row]) {
+		if table[row][col] == '#' {
+			row, col = reverse(row, col, direction)
+			direction = turnRight(direction)
+		} else {
+			pos := pos{row, col, direction}
+			if positions.Contains(pos) {
+				return true
+			}
+			positions.Add(pos)
+		}
+		row, col = advance(row, col, direction)
+	}
+	return false
 }
 
 func turnRight(direction int) int {
@@ -85,17 +140,19 @@ func advance(row, col, direction int) (int, int) {
 	panic("invalid direction")
 }
 
-func findStart(table [][]uint8) (int, int) {
+func findStart(table [][]uint8) (int, int, int) {
 	for i := 0; i < len(table); i++ {
 		for j := 0; j < len(table[i]); j++ {
 			if table[i][j] == '^' {
-				return i, j
+				return i, j, Up
+			} else if table[i][j] == '>' {
+				return i, j, Right
+			} else if table[i][j] == 'v' {
+				return i, j, Down
+			} else if table[i][j] == '<' {
+				return i, j, Left
 			}
 		}
 	}
 	panic("no start position found")
-}
-
-func Part2(filename string) int64 {
-	return 0
 }
