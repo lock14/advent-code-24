95c9470985b3e717de4c123062c33095593c3013
Brian Bechtel <bbechtel@google.com>
2024-12-09 22:01:28 -0800

    Day 10: Part 2

--- day10/day10.go
+++ day10/day10.go
@@ -28,6 +28,24 @@ func Part1(filename string) int64 {
 	return count
 }
 
+func Part2(filename string) int64 {
+	var table []string
+	scanner, closeFunc := NewScanner(filename)
+	defer closeFunc()
+	for scanner.Scan() {
+		table = append(table, scanner.Text())
+	}
+	var count int64
+	for i := 0; i < len(table); i++ {
+		for j := 0; j < len(table[i]); j++ {
+			if table[i][j] == '0' {
+				count += trailHeadRating(table, i, j, '0')
+			}
+		}
+	}
+	return count
+}
+
 func trailHeadScore(table []string, i int, j int, target byte, summitPos map[int]map[int]struct{}) {
 	if i < 0 || i >= len(table) || j < 0 || j >= len(table[i]) {
 		return
@@ -45,6 +63,17 @@ func trailHeadScore(table []string, i int, j int, target byte, summitPos map[int
 	}
 }
 
-func Part2(filename string) int64 {
+func trailHeadRating(table []string, i int, j int, target byte) int64 {
+	if i < 0 || i >= len(table) || j < 0 || j >= len(table[i]) {
+		return 0
+	}
+	if table[i][j] == target && target == '9' {
+		return 1
+	} else if table[i][j] == target {
+		return trailHeadRating(table, i-1, j, target+1) + // up
+			trailHeadRating(table, i+1, j, target+1) + // down
+			trailHeadRating(table, i, j-1, target+1) + // left
+			trailHeadRating(table, i, j+1, target+1) // right
+	}
 	return 0
 }
